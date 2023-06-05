af418ddf112231cd411cd48abdc83084a344d41d
Isabel <isabel.d.johansson@gmail.com>
2023-06-05 17:00:15 +0200

    First additions.

---
+++ Source/PacPong/Pac.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Pac.generated.h"
+
+class UFloatingPawnMovement;
+class USphereComponent;
+
+UCLASS()
+class PACPONG_API APac : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	APac();
+
+	UPROPERTY(EditAnywhere)
+	USphereComponent* SphereComponent;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MeshComponent;
+
+	UFUNCTION()
+	void OnPacHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	UFUNCTION()
+	void OnPacOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UFloatingPawnMovement* PacMovement;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
