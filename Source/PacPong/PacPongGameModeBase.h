6fbb04a1132acbd244ee776eb629591331753204
Isabel <isabel.d.johansson@gmail.com>
2023-06-05 11:18:02 +0200

    Project added to repo.

---
+++ Source/PacPong/PacPongGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PacPongGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PACPONG_API APacPongGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
